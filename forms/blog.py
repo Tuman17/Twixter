7562e213c617ceb04327baa031967a1ce86a7865
TumanZ <tumanzplay17@yandex.ru>
2024-04-29 14:08:36 +0300

    Release

---
+++ forms/blog.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class BlogForm(FlaskForm):
+    title = StringField('Название', [DataRequired(), Length(min=3, max=36)])
+    content = TextAreaField('Содержание')
+    submit = SubmitField('Опубликовать')
